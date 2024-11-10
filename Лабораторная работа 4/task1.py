78ca3984252a62dd628a14654ee5b7a6eb72a88e
klemac <62691087+klemac@users.noreply.github.com>
2024-11-11 02:01:03 +0300

    Lab 4

---
+++ Лабораторная работа 4/task1.py
@@ -0,0 +1,15 @@
+# TODO решите задачу
+import json
+
+FILENAME = 'input.json'
+
+
+def task() -> float:
+    with open(FILENAME) as f:
+        data = json.load(f)
+
+    sum_of_values = sum(item['score'] * item['weight'] for item in data)
+    return round(sum_of_values, 3)
+
+
+print(task())
